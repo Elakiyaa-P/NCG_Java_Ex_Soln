a64be048cd9e8fd98e72b93f141c7f1ab85a2c76
Elakiyaa <elakiyaa5118@gmail.com>
2020-11-13 22:17:56 +0530

    Modified PaintJob

--- src/com/company/PaintJob.java
+++ src/com/company/PaintJob.java
@@ -1,26 +1,33 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class PaintJob {
-    public static int getBucketCount(double width, double height, double areaPerBucket, double extraBuckets){
-        if(width<1 || height<1 || areaPerBucket<1 || extraBuckets<0){
+    public static int getBucketCount(double width, double height, double areaPerBucket, double extraBuckets) {
+        if (width < 1 || height < 1 || areaPerBucket < 1 || extraBuckets < 0) {
             return -1;
         }
-        return (int)extraBuckets - (int) Math.ceil((width*height)/areaPerBucket);
+        return (int) extraBuckets - (int) Math.ceil((width * height) / areaPerBucket);
     }
-    public static int getBucketCount(double width, double height, double areaPerBucket){
-        if(width<1 || height<1 || areaPerBucket<1){
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        if (width < 1 || height < 1 || areaPerBucket < 1) {
             return -1;
         }
-        return (int) Math.ceil((width*height)/areaPerBucket);
+        return (int) Math.ceil((width * height) / areaPerBucket);
     }
-    public static int getBucketCount(double area, double areaPerBucket){
-        if(area<1 || areaPerBucket<1){
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area < 1 || areaPerBucket < 1) {
             return -1;
         }
-        return (int) Math.ceil((area)/areaPerBucket);
+        return (int) Math.ceil((area) / areaPerBucket);
     }
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(getBucketCount(scanner.nextDouble(),scanner.nextDouble(),scanner.nextDouble(),scanner.nextDouble()));
+        System.out.println(getBucketCount(scanner.nextDouble(),scanner.nextDouble(),scanner.nextDouble()));
+        System.out.println(getBucketCount(scanner.nextDouble(),scanner.nextDouble()));
     }
 }
